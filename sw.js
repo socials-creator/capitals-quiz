1be57290cfdc93af86291105bbf585f7d3380f30
socials-creator <me@nihar.ca>
2026-08-14 20:27:43 -0400

    V3

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE="capital-rush-v2";
+const CACHE="capital-rush-v3";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener("install", event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener("activate", event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener("fetch", event=>{
+  if(new URL(event.request.url).pathname.endsWith("/sw.js")) return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+});
