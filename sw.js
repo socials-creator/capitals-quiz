14f48e4bc4078f986fd1b669a6324665a6e204d7
socials-creator <me@nihar.ca>
2026-08-14 20:57:54 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,68 @@
-const CACHE="capital-rush-v4";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png"];
+const CACHE = "capital-rush-v5";
 
-self.addEventListener("install", event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-180.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+
 });
 
-self.addEventListener("activate", event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+
+  );
+
 });
 
-self.addEventListener("fetch", event=>{
-  if(new URL(event.request.url).pathname.endsWith("/sw.js")) return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+
+self.addEventListener("fetch", event => {
+
+  if(
+    new URL(event.request.url)
+      .pathname
+      .endsWith("/sw.js")
+  ){
+    return;
+  }
+
+  event.respondWith(
+
+    caches
+      .match(event.request)
+      .then(cached => {
+
+        if(cached){
+          return cached;
+        }
+
+        return fetch(event.request);
+
+      })
+
+  );
+
 });
