41dd4bba34be087d899502e0c9596c0b6ae05a68
socials-creator <me@nihar.ca>
2026-08-14 20:19:16 -0400

    V2
    No check answer button except when multiple correct

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="capital-rush-v1";
+const CACHE="capital-rush-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
